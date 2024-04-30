365ce81921cca8832901033efa364e23aaa937ba
mdparvajmosharof <mdparvajmosharof17@gmail.com>
2024-04-30 21:58:14 +0600

    country card update

--- src/Pages/SCountry.jsx
+++ src/Pages/SCountry.jsx
@@ -4,28 +4,28 @@ const SCountry = ({sCountry}) => {
 
     const {image, countryName, countryDescription} = sCountry;
 
-  // return (
-  //   <div>
+  return (
+    <div>
         
-  //    <Link to={`/country/${countryName}`}>
-  //    <div className="card w-96 bg-base-100 shadow-xl">
-  //       <figure>
-  //         <img
-  //           src={image}
-  //           alt="Shoes"
-  //         />
-  //       </figure>
-  //       <div className="card-body">
-  //         <h2 className="card-title">{countryName}</h2>
-  //         <p>{countryDescription}</p>
-  //         {/* <div className="card-actions justify-end">
-  //           <button className="btn btn-primary">Buy Now</button>
-  //         </div> */}
-  //       </div>
-  //     </div>
-  //    </Link>
-  //   </div>
-  // );
+     <Link to={`/country/${countryName}`}>
+     <div className="card w-96 bg-base-100 shadow-xl">
+        <figure>
+          <img
+            src={image}
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{countryName}</h2>
+          <p>{countryDescription}</p>
+          {/* <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div> */}
+        </div>
+      </div>
+     </Link>
+    </div>
+  );
 };
 
 export default SCountry;
