ce56675752153695e8c8a4f4f4f6e726b0a63374
mdparvajmosharof <mdparvajmosharof17@gmail.com>
2024-04-30 21:49:08 +0600

    all toiurists spots update

--- src/Pages/AllMySpot.jsx
+++ src/Pages/AllMySpot.jsx
@@ -4,47 +4,47 @@ import { IoMdTime } from "react-icons/io";
 import { TbEyeCheck } from "react-icons/tb";
 import { Link } from "react-router-dom";
 
-const AllMySpot = ({ mySpot }) => {
-  const { image, touristsSpotName, cost, season, time, visitor, _id } = mySpot;
-  return (
-    <div>
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <figure>
-          <img className=" h-[250px]" src={image} alt="Shoes" />
-        </figure>
-        <div className="card-body space-y-3">
-          <div className="flex justify-between items-center">
-            <div>
-              <h2 className="card-title">{touristsSpotName}</h2>
-            </div>
-            <div className="flex justify-start gap-2 items-center text-sm">
-              <TiWeatherCloudy />
-              <span>{season}</span>
-            </div>
-          </div>
-          <div className="flex justify-between">
-            <div className="flex justify-start gap-2 items-center text-sm font-bold text-blue-700">
-              $<span>{cost}</span>
-            </div>
-            <div className="flex justify-start gap-2 items-center text-sm">
-              <IoMdTime />
-              <span>{time}</span>
-            </div>
-            <div className="flex justify-start gap-2 items-center text-sm">
-              <TbEyeCheck />
-              <span>{visitor}</span>
-            </div>
-          </div>
-          <div className="card-actions">
-            <Link to={`/SpotDetails/${_id}`} className="w-full">
-              {" "}
-              <button className="btn btn-primary w-full">Details</button>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+// const AllMySpot = ({ mySpot }) => {
+//   const { image, touristsSpotName, cost, season, time, visitor, _id } = mySpot;
+//   return (
+//     <div>
+//       <div className="card w-96 bg-base-100 shadow-xl">
+//         <figure>
+//           <img className=" h-[250px]" src={image} alt="Shoes" />
+//         </figure>
+//         <div className="card-body space-y-3">
+//           <div className="flex justify-between items-center">
+//             <div>
+//               <h2 className="card-title">{touristsSpotName}</h2>
+//             </div>
+//             <div className="flex justify-start gap-2 items-center text-sm">
+//               <TiWeatherCloudy />
+//               <span>{season}</span>
+//             </div>
+//           </div>
+//           <div className="flex justify-between">
+//             <div className="flex justify-start gap-2 items-center text-sm font-bold text-blue-700">
+//               $<span>{cost}</span>
+//             </div>
+//             <div className="flex justify-start gap-2 items-center text-sm">
+//               <IoMdTime />
+//               <span>{time}</span>
+//             </div>
+//             <div className="flex justify-start gap-2 items-center text-sm">
+//               <TbEyeCheck />
+//               <span>{visitor}</span>
+//             </div>
+//           </div>
+//           <div className="card-actions">
+//             <Link to={`/SpotDetails/${_id}`} className="w-full">
+//               {" "}
+//               <button className="btn btn-primary w-full">Details</button>
+//             </Link>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
 export default AllMySpot;
